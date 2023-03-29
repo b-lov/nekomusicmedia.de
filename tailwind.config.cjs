13d485919c942ea9e9fbdf4234e4f2341ffcdae0
b-lov <max@bilov.de>
2023-03-29 13:12:27 +0200

    added fonts to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
+        oswald: ['Oswald']
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
 };
