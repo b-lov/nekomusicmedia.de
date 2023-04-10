1316fb7703ff4dd8549e579a0c6a4737ee212b33
b-lov <max@bilov.de>
2023-04-10 22:09:58 +0200

    change google map locale

--- src/routes/[lang=lang]/contact/GoogleMap.svelte
+++ src/routes/[lang=lang]/contact/GoogleMap.svelte
@@ -1,7 +1,11 @@
+<script>
+  import { locale } from '$i18n/i18n-svelte';
+</script>
+
 <div class="w-full h-80 shadow-lg bg-red-50">
   <iframe
     title="map"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2503.2988454565125!2d6.409345951358126!3d51.139839979476214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bf5482cc430f45%3A0x45e756d453bbc706!2sNeko%20Music%20%26%20Media%20GmbH!5e0!3m2!1sde!2sde!4v1659985729919!5m2!1sde!2sde"
+    src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2503.2988454565125!2d6.409345951358126!3d51.139839979476214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bf5482cc430f45%3A0x45e756d453bbc706!2sNeko%20Music%20%26%20Media%20GmbH!5e0!3m2!1s${$locale}!2s${$locale}!4v1659985729919!5m2!1s${$locale}!2s${$locale}`}
     style="border:0;"
     width="100%"
     height="100%"
